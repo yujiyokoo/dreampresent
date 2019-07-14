5d21b634e774c0cd4aba507ed587aa8fb2d3cbda
Yuji Yokoo <yujiyokoo@gmail.com>
2019-07-14 05:20:32 +0000

    Add some example data, remove comments

--- src/page_data.rb
+++ src/page_data.rb
@@ -4,26 +4,32 @@ class BasePage
   end
 end
 
-class Page1 < BasePage
-  def render(dc_kos)
-    render_bg(dc_kos)
-    dc_kos.test_png("/rd/Dreamcast.png", 100, 100)
-    dc_kos.draw_str("Hello")
-  end
-end
+PageClasses = []
 
-class Page2 < BasePage
+PageClasses.push (Class.new(BasePage) do
   def render(dc_kos)
     render_bg(dc_kos)
-    dc_kos.test_png("/rd/Dreamcast.png", 200, 200)
-    dc_kos.draw_str("Hola")
+    dc_kos.console_print("----------------")
+    dc_kos.test_png("/rd/Dreamcast.png", 100, 100)
+    dc_kos.draw_str("Developing Dreamcast games with mruby", 60, 30)
+    dc_kos.draw_str("Yuji Yokoo - @yujiyokoo", 60, 60)
   end
-end
+end).new
+
+PageClasses.push (Class.new(BasePage) do
+  def render(dc_kos)
+    #render_bg(dc_kos)
+    dc_kos.test_png("/rd/test_image.png", 0, 0) # alternative bg
+    dc_kos.console_print("----------------")
+    dc_kos.draw_str("About me (Yuji)", 60, 30 )
+    dc_kos.draw_str("Yuji Yokoo - Software developer", 60, 60 )
+    dc_kos.draw_str("15 years ago: Win32/MFC Desktop application developer", 60, 80 )
+    dc_kos.draw_str("Likes: Ruby, Console games", 60, 120 )
+  end
+end).new
 
 class PageData
   def self.all
-    [ Page1.new,
-      Page2.new
-    ]
+    PageClasses
   end
 end
--- src/presentation.rb
+++ src/presentation.rb
@@ -5,14 +5,7 @@ class Page
   end
 
   def render
-    #puts "rendering page"
-    #puts "about to load png"
-    #@dc_kos.load_bg_png("/rd/test_image_512x512.png")
-    #puts "done load png"
-
     @page_data.render(@dc_kos)
-    #@dc_kos.test_png("/rd/Dreamcast.png", 100, 100)
-    #@dc_kos.draw_str("Hello")
   end
 end
 
@@ -26,7 +19,6 @@ class Presentation
 
   def wait_for_next_page_button
     while true do
-      @dc_kos.draw_str("Waiting...")
       rand(1) # hopefully this would give us a "more random" start point
       button_state = @dc_kos::get_button_state
 
@@ -35,10 +27,6 @@ class Presentation
   end
 
   def run
-   # @dc_kos.load_bg_png("/rd/test_image_512x512.png")
-   #   wait_for_next_page_button
-   # @dc_kos.test_png("/rd/test_image.png", 0, 0)
-   #   wait_for_next_page_button
     @pages.each { |page|
       page.render
       wait_for_next_page_button
